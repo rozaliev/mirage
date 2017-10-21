e36a95e2f719a102a2484550d850366acf564971
Evgeny Rozaliev <rozaliev@gmail.com>
2017-10-21 21:30:07 +0300

    [core] core accepts immovable generators

--- mirage-core/src/lib.rs
+++ mirage-core/src/lib.rs
@@ -1,10 +1,17 @@
+#![feature(immovable_types)]
+use std::marker::Move;
+
 extern crate mirage_async;
 use mirage_async::{Async, Await};
 
 pub struct Core;
 
 impl Core {
-    pub fn run<T, A: Async<T>>(&mut self, mut a: A) -> T {
+    pub fn new() -> Core {
+        Core
+    }
+
+    pub fn run<T, A: Async<T> + ?Move>(&mut self, mut a: A) -> T {
         loop {
             if let Await::Done(r) = a.poll() {
                 return r;
